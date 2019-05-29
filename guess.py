983edc62c2e9510d4475c17eae40c3f335e299dc
Chenbo Yi <yichenbo@outlook.com>
2019-05-29 21:18:27 +0100

    add the example of list(phoneNumber) and example of error handelling(ErrorHandelling)

--- guess.py
+++ guess.py
@@ -2,10 +2,11 @@ guess_count = 0
 guess_limit = 3
 secrete_number = 9
 
-while guess_count<guess_limit:
+while guess_count < guess_limit:
     guess = int(input('guess a number: '))
     guess_count += 1
     if guess == secrete_number:
         print('You Won!')
         break
-else: print('You Failed!')
+else:
+    print('You Failed!')
